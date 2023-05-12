07dcfa04c8539b6292ba4b27d663402a27df83d0
MrS-E <simeon.stix@gmx.ch>
2023-05-12 09:28:30 +0200

    add db setup http script and add validator

---
+++ Datenbank/validator.js
@@ -0,0 +1,14 @@
+function(newDoc, oldDoc, userCtx) {
+    function require(field, message) {
+        message = message || "Document must have a " + field;
+        if (!newDoc[field]) throw({forbidden : message});
+    }
+    if(newDoc){
+        require("name");
+        require("category");
+        require("date");
+        require("ingredient");
+        require("manual");
+        require("image");
+    }
+}
