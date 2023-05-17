b53ecc5a1cddfbd26189ecf8c69ed7274dcc84d2
MrS-E <simeon.stix@gmx.ch>
2023-05-17 19:21:16 +0200

    add onclick

--- Code/rezept/resources/views/home.blade.php
+++ Code/rezept/resources/views/home.blade.php
@@ -4,7 +4,7 @@
 @section('main')
     <div class="row">
         @foreach($recipes as $recipe)
-            <div class="col-4 mt-2" onclick="window.location.assign('{{"/".$recipe->_id}}')">
+            <div class="col-lg-3 col-md-4 col-sm-6 mt-2" onclick="window.location.assign('{{"/".$recipe->_id}}')">
                 <img src="{{$recipe->image}}" class="img-fluid rounded-4"/>
                 <p class="text-center"><strong>{{$recipe->name}}</strong></p>
             </div>
