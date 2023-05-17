a4af7ff9f436f13baf6955588f09a1d3cb5c583f
MrS-E <simeon.stix@gmx.ch>
2023-05-17 14:25:20 +0200

    first version of main page

--- Code/rezept/resources/views/home.blade.php
+++ Code/rezept/resources/views/home.blade.php
@@ -4,8 +4,9 @@
 @section('main')
     <div class="row">
         @foreach($recipes as $recipe)
-            <div class="col-4">
-                {{$recipe}}
+            <div class="col-4 mt-2" onclick="window.location.assign('{{"/".$recipe->_id}}')">
+                <img src="{{$recipe->image}}" class="img-fluid rounded-4"/>
+                <p class="text-center"><strong>{{$recipe->name}}</strong></p>
             </div>
         @endforeach
     </div>
