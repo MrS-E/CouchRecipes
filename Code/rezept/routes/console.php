459e936579dcd5255859bfcbae4600ee7a618e2c
MrS-E <simeon.stix@gmx.ch>
2023-06-17 15:48:15 +0200

    working on a command

--- Code/rezept/routes/console.php
+++ Code/rezept/routes/console.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () { //I keep that function because I like it
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+//more in /app/Console/Commands
